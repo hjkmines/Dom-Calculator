b8e8c8a740b678009f3cc37e3dffea6d9a55fa7a
Tony Kim <hjkmines@gmail.com>
2020-07-25 12:53:35 -0600

    adds number feature to screen

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const clear = document.querySelector("#clear")
 const screen = document.querySelector("#screen")
-
-screen.textContent = 3
-
+const buttons = document.querySelectorAll("span")
 
 clear.addEventListener("click", () => {
   screen.textContent = ""
 })
+
+buttons.forEach(node => {
+  if (node.textContent == 0 || node.textContent == 1 || node.textContent == 2 || node.textContent == 3 || node.textContent == 4 || node.textContent == 5 || node.textContent == 6 || node.textContent == 7 || node.textContent == 8 || node.textContent == 9) {
+    node.classList.add("number")
+  }
+})
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach(number => {
+  number.addEventListener("click", event => {
+    screen.textContent = event.target.textContent
+  })
+})
+
+
